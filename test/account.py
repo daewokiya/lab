02cc6629c66fd4655739c1ad53307b9792a04b5e
daewokiya <118556382+daewokiya@users.noreply.github.com>
2023-04-21 13:49:18 -0500

    Update account.py

--- test/account.py
+++ test/account.py
@@ -1,9 +1,17 @@
 class Account:
     def __init__(self, name: str) -> None:
+        '''
+        init function declares name and balance
+        '''
         self.__account_name = name
         self.__account_balance = 0
 
     def deposit(self, amount: float):
+        '''
+        Function takes a value from amount and tests to see if its valid then adds it to account balance
+        :param amount (float): This is the value added to balance
+        :return: Relays if the value went through or not
+        '''
         if amount > 0:
             self.__account_balance = float(self.__account_balance + amount)
             return True
@@ -11,6 +19,11 @@ class Account:
             return False
 
     def withdraw(self, amount: float):
+        '''
+        Function takes a value from amount and tests to see if its valid and then subtracts it from the balance
+        :param amount (float): This is the value taken from balance
+        :return: Relays if the value went through or not
+        '''
         if amount > 0:
             if amount<= self.__account_balance:
                 self.__account_balance = float(self.__account_balance - amount)
@@ -21,7 +34,15 @@ class Account:
             return False
 
     def get_balance(self) -> str:
+        '''
+        Function gets the balance of the account
+        :return: Gets the balance of the account
+        '''
         return self.__account_balance
 
     def get_name(self) -> str:
+        '''
+        Function gets the name of the account
+        :return: Gets the name of the account
+        '''
         return self.__account_name
